87685206c1e2f56bfa11ff50b7958b6a16e0149e
Warren R Bank <warren.r.bank@gmail.com>
2021-09-02 02:55:05 -0700

    v1.0.0

---
+++ README.md
@@ -0,0 +1,13 @@
+### [Radio.net](https://github.com/warren-bank/crx-Radio-Net/tree/webmonkey-userscript/es5)
+
+[Userscript](https://github.com/warren-bank/crx-Radio-Net/raw/webmonkey-userscript/es5/webmonkey-userscript/Radio-Net.user.js) to run in both:
+* the [WebMonkey](https://github.com/warren-bank/Android-WebMonkey) application for Android
+* the [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo) web browser extension for Chrome/Chromium
+
+Its purpose is to:
+* redirect embedded audio streams from [radio.net](https://www.radio.net/) to an external player
+
+#### Legal:
+
+* copyright: [Warren Bank](https://github.com/warren-bank)
+* license: [GPL-2.0](https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt)
---
+++ webmonkey-userscript/Radio-Net.user.js
@@ -0,0 +1,171 @@
+// ==UserScript==
+// @name         Radio.net
+// @description  Listen to audio streams in external player.
+// @version      1.0.0
+// @match        *://radio.net/s/*
+// @match        *://*.radio.net/s/*
+// @icon         https://www.radio.net/assets/fav/favicon-32x32.png
+// @run-at       document-end
+// @grant        unsafeWindow
+// @homepage     https://github.com/warren-bank/crx-Radio-Net/tree/webmonkey-userscript/es5
+// @supportURL   https://github.com/warren-bank/crx-Radio-Net/issues
+// @downloadURL  https://github.com/warren-bank/crx-Radio-Net/raw/webmonkey-userscript/es5/webmonkey-userscript/Radio-Net.user.js
+// @updateURL    https://github.com/warren-bank/crx-Radio-Net/raw/webmonkey-userscript/es5/webmonkey-userscript/Radio-Net.user.js
+// @namespace    warren-bank
+// @author       Warren Bank
+// @copyright    Warren Bank
+// ==/UserScript==
+
+// ----------------------------------------------------------------------------- constants
+
+var user_options = {
+  "redirect_to_webcast_reloaded": true,
+  "force_http":                   true,
+  "force_https":                  false,
+
+  "timeout_ms": {
+    "live_audiostream":           0
+  }
+}
+
+// ----------------------------------------------------------------------------- URL links to tools on Webcast Reloaded website
+
+var get_webcast_reloaded_url = function(video_url, vtt_url, referer_url, force_http, force_https) {
+  force_http  = (typeof force_http  === 'boolean') ? force_http  : user_options.force_http
+  force_https = (typeof force_https === 'boolean') ? force_https : user_options.force_https
+
+  var encoded_video_url, encoded_vtt_url, encoded_referer_url, webcast_reloaded_base, webcast_reloaded_url
+
+  encoded_video_url     = encodeURIComponent(encodeURIComponent(btoa(video_url)))
+  encoded_vtt_url       = vtt_url ? encodeURIComponent(encodeURIComponent(btoa(vtt_url))) : null
+  referer_url           = referer_url ? referer_url : unsafeWindow.location.href
+  encoded_referer_url   = encodeURIComponent(encodeURIComponent(btoa(referer_url)))
+
+  webcast_reloaded_base = {
+    "https": "https://warren-bank.github.io/crx-webcast-reloaded/external_website/index.html",
+    "http":  "http://webcast-reloaded.surge.sh/index.html"
+  }
+
+  webcast_reloaded_base = (force_http)
+                            ? webcast_reloaded_base.http
+                            : (force_https)
+                               ? webcast_reloaded_base.https
+                               : (video_url.toLowerCase().indexOf('http:') === 0)
+                                  ? webcast_reloaded_base.http
+                                  : webcast_reloaded_base.https
+
+  webcast_reloaded_url  = webcast_reloaded_base + '#/watch/' + encoded_video_url + (encoded_vtt_url ? ('/subtitle/' + encoded_vtt_url) : '') + '/referer/' + encoded_referer_url
+  return webcast_reloaded_url
+}
+
+// ----------------------------------------------------------------------------- URL redirect
+
+var redirect_to_url = function(url) {
+  if (!url) return
+
+  if (typeof GM_loadUrl === 'function') {
+    if (typeof GM_resolveUrl === 'function')
+      url = GM_resolveUrl(url, unsafeWindow.location.href)
+
+    GM_loadUrl(url, 'Referer', unsafeWindow.location.href)
+  }
+  else {
+    try {
+      unsafeWindow.top.location = url
+    }
+    catch(e) {
+      unsafeWindow.window.location = url
+    }
+  }
+}
+
+var process_video_url = function(video_url, video_type, vtt_url, referer_url) {
+  if (!referer_url)
+    referer_url = unsafeWindow.location.href
+
+  if (typeof GM_startIntent === 'function') {
+    // running in Android-WebMonkey: open Intent chooser
+
+    var args = [
+      /* action = */ 'android.intent.action.VIEW',
+      /* data   = */ video_url,
+      /* type   = */ video_type
+    ]
+
+    // extras:
+    if (vtt_url) {
+      args.push('textUrl')
+      args.push(vtt_url)
+    }
+    if (referer_url) {
+      args.push('referUrl')
+      args.push(referer_url)
+    }
+
+    GM_startIntent.apply(this, args)
+    return true
+  }
+  else if (user_options.redirect_to_webcast_reloaded) {
+    // running in standard web browser: redirect URL to top-level tool on Webcast Reloaded website
+
+    redirect_to_url(get_webcast_reloaded_url(video_url, vtt_url, referer_url))
+    return true
+  }
+  else {
+    return false
+  }
+}
+
+var process_hls_url = function(hls_url, vtt_url, referer_url) {
+  process_video_url(/* video_url= */ hls_url, /* video_type= */ 'application/x-mpegurl', vtt_url, referer_url)
+}
+
+var process_dash_url = function(dash_url, vtt_url, referer_url) {
+  process_video_url(/* video_url= */ dash_url, /* video_type= */ 'application/dash+xml', vtt_url, referer_url)
+}
+
+var process_mp3_url = function(mp3_url, vtt_url, referer_url) {
+  process_video_url(/* video_url= */ mp3_url, /* video_type= */ 'audio/mpeg', vtt_url, referer_url)
+}
+
+// ----------------------------------------------------------------------------- process video within iframe
+
+var process_live_audiostream = function() {
+  var regex, scripts, script, audio_url
+
+  regex = {
+    whitespace: /[\r\n\t]+/g,
+    audio_url:  /^.*['"]streams['"]\s*:[\[\{\s]+['"]url['"]\s*:\s*['"]([^'"]+)['"][,\s]+['"]contentFormat['"]\s*:\s*['"]audio\/.*$/
+  }
+
+  scripts = unsafeWindow.document.querySelectorAll('script[suppressHydrationWarning]:not([src])')
+
+  for (var i=0; i < scripts.length; i++) {
+    script = scripts[i]
+    script = script.innerHTML
+    script = script.replace(regex.whitespace, ' ')
+
+    if (regex.audio_url.test(script)) {
+      audio_url = script.replace(regex.audio_url, '$1') + '#audio.mp3'
+      break
+    }
+  }
+
+  if (!audio_url)
+    return
+
+  process_mp3_url(audio_url)
+}
+
+// ----------------------------------------------------------------------------- bootstrap
+
+var init = function() {
+  if (user_options.timeout_ms.live_audiostream > 0)
+    setTimeout(process_live_audiostream, user_options.timeout_ms.live_audiostream)
+  else
+    process_live_audiostream()
+}
+
+init()
+
+// -----------------------------------------------------------------------------
